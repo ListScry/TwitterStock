57da0b3540c984a975aebc6a076c1eb9a57b64de
Kadish <kadish@server.fake>
2014-02-24 20:58:04 -0600

    rolled back; recommitting without models jar

--- src/SentimentAnalysis/SentimentAnalyzer.java
+++ src/SentimentAnalysis/SentimentAnalyzer.java
@@ -5,7 +5,7 @@ import java.util.Properties;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-//import edu.stanford.nlp.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.rnn.RNNCoreAnnotations;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.CoreMap;
@@ -27,14 +27,14 @@ public class SentimentAnalyzer {
             Annotation annotation = pipeline.process(line);
             for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
                 Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
-                /*
+
                 int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
                 String partText = sentence.toString();
                 if (partText.length() > longest) {
                     mainSentiment = sentiment;
                     longest = partText.length();
                 }
-                */
+
             }
         }
         if (mainSentiment == 2 || mainSentiment > 4 || mainSentiment < 0) {
