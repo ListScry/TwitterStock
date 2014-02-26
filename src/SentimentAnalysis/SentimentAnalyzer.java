1ee6fbccba79625fda351e3fdf85e850b927f803
Kadish <kadish@server.fake>
2014-02-26 12:49:37 -0600

    switched to just return float for sentiment

--- src/SentimentAnalysis/SentimentAnalyzer.java
+++ src/SentimentAnalysis/SentimentAnalyzer.java
@@ -16,56 +16,34 @@ import edu.stanford.nlp.util.CoreMap;
 
 public class SentimentAnalyzer {
 
-    public TweetWithSentiment findSentiment(String line) {
+    public float findSentiment(String line) {
 
         Properties props = new Properties();
         props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        int mainSentiment = 0;
+        float totalSentiment = 0.0f;
+        int count = 0;
         if (line != null && line.length() > 0) {
             int longest = 0;
             Annotation annotation = pipeline.process(line);
             for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
                 Tree tree = sentence.get(SentimentCoreAnnotations.AnnotatedTree.class);
+                System.out.println(" " + tree.toString());
 
                 int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
-                String partText = sentence.toString();
-                if (partText.length() > longest) {
-                    mainSentiment = sentiment;
-                    longest = partText.length();
-                }
 
+                totalSentiment += sentiment;
+                count++;
             }
         }
-        if (mainSentiment == 2 || mainSentiment > 4 || mainSentiment < 0) {
-            return null;
-        }
-        TweetWithSentiment tweetWithSentiment = new TweetWithSentiment(line, toCss(mainSentiment));
-        return tweetWithSentiment;
+        return totalSentiment/count;
 
-    }
-
-    private String toCss(int sentiment) {
-        switch (sentiment) {
-            case 0:
-                return "alert alert-danger";
-            case 1:
-                return "alert alert-danger";
-            case 2:
-                return "alert alert-warning";
-            case 3:
-                return "alert alert-success";
-            case 4:
-                return "alert alert-success";
-            default:
-                return "";
-        }
     }
 
     public static void main(String[] args) {
         SentimentAnalyzer sentimentAnalyzer = new SentimentAnalyzer();
-        TweetWithSentiment tweetWithSentiment = sentimentAnalyzer
-                .findSentiment("happy good awesome! this tweet is really positive!");
-        System.out.println(tweetWithSentiment);
+        float sent = sentimentAnalyzer
+                .findSentiment("meh! this tweet is really positive!");
+        System.out.println(sent);
     }
 }
