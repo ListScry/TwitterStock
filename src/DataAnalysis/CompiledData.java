451b641bd302454d9ab2cdd65316d78d8a8ae09d
Kadish <kadish@server.fake>
2014-03-04 23:53:04 -0600

    very minor update

--- src/DataAnalysis/CompiledData.java
+++ src/DataAnalysis/CompiledData.java
@@ -18,12 +18,13 @@ public class CompiledData {
     private ArrayList<ArrayList<TweetData> > tweetBuckets;
     private ArrayList<ArrayList<YQLHistoricalData> > stockBuckets;
 
+    private static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
     /* CONSTRUCTOR
         start and end in format "yyyy-MM-dd"
      */
     public CompiledData(ArrayList<TweetData> tweets, ArrayList<YQLHistoricalData> stocks, String start, String end){
         //format for start and end datestamps
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         try{
             startDate = formatter.parse(start);
             endDate = formatter.parse(end);
@@ -39,5 +40,36 @@ public class CompiledData {
         stockBuckets = new ArrayList<ArrayList<YQLHistoricalData> >(2*(int)numDays);
 
         //loop through passed-in data and put into the appropriate buckets
+        //check timezone information?
+    }
+
+    public float getMood(String timestamp){
+
+        return 0.0f;
+    }
+
+    public ArrayList<TweetData> getTweets(String timestamp){
+        return null;
+    }
+
+    public ArrayList<YQLHistoricalData> getStockData(String timestamp){
+        return null;
+    }
+
+    // timestamp in format "yyyy-MM-dd:HH-mm-ss"
+    private int indexFromTimestamp(String timestamp){
+        long secsFromStart = -1;
+        try{
+            Date tsDate = formatter.parse(timestamp);
+            secsFromStart = (tsDate.getTime()-startDate.getTime())/1000;
+        }
+        catch (ParseException pe) {
+            pe.printStackTrace();
+            return -1;
+        }
+
+        //fill in calcs
+
+        return 0;
     }
 }
