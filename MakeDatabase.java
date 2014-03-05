86dda3a3fe9a0e4f1f095aa193aede3726667695
Stephen N. Lee <leesn@stolaf.edu>
2014-03-04 18:55:34 -0600

    Wrote database code that seems to work and started an actual database. I need to talk with the others before I interface with them (plus Jon probably knows more about SQL dates).

--- MakeDatabase.java
+++ MakeDatabase.java
@@ -9,11 +9,26 @@ public class MakeDatabase {
 	String s = "CREATE TABLE Quote ("
 	    + "ID varchar(30), Ticker varchar(5), Timestamp DATETIME," +
 	    "Mood varchar(30), Keyword varchar(30), Volume int, Adj_close Decimal(4,2) );";
+
+	String i = "INSERT INTO Quote VALUES ('alphanotomega', 'CKG', " +
+	    "'1970-01-01 00:00:01', 'excited', 'violin', 42, 0002.44)";
+
+	String q = "SELECT Volume FROM Quote";
 	try {
 	    db.open(true);
-	    SQLiteStatement st = db.prepare(s);
+	    SQLiteStatement st;
+	    //st = db.prepare(s);
+	    //st.step();
+	    //st.dispose();
+	    st = db.prepare(i);
 	    st.step();
 	    st.dispose();
-	} catch (SQLiteException ex) {}
+	    st = db.prepare(q);
+	    if(st.step()) {
+		System.out.println(st.columnInt(0));
+	    }
+	} catch (SQLiteException ex) {
+	    ex.printStackTrace();
+	}
     }
 }
